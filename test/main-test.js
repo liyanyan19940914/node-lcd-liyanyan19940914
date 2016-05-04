71070d000fd5d204205dfbda5e79a657da8920d3
liyanyan19940914 <921258351@qq.com>
2016-05-04 21:38:48 +0800

    second node

--- test/main-test.js
+++ test/main-test.js
@@ -1,31 +1,45 @@
 var main = require('../main/main.js');
-var fixtures = require('./fixtures.js');
+var fixtures = require('../main/lcdDigits.js');
 
-describe('unit testing',function () {
+describe('test main',function () {
     var allLcds;
     beforeEach(function () {
         allLcds= fixtures.loadAllLcds();
     });
+    it('return correct print',function () {
+        spyOn(console,'log');
+        main.createLcd(910);
+        var print='._. ... ._. \n'+'|_| ..| |.| \n'+'..| ..| |_| ';
+        expect(console.log).toHaveBeenCalledWith(print);
+    });
 
     describe('test splitNumber', function () {
+        var input=910;
         it('return correct numberArray', function () {
-            var numberArray = main.splitNumber(910);
+            var numberArray = fixtures.buildArray(input);
             expect(numberArray).toEqual([9, 1, 0]);
         });
     });
 
     describe('test buildLcdArray', function () {
+        var numberArray;
+        beforeEach(function () {
+            numberArray=[9,1,0];
+        });
 
         it('return correct lcdArray',function () {
-            expect(main.buildLcdArray([9,1,0],allLcds)).toEqual(['._. ... ._. ','|_| ..| |.| ','..| ..| |_| '])
+            var allLcds=fixtures.loadAllLcds();
+            var lcdArray=[['._.', '|_|', '..|'], ['...', '..|', '..|'], ['._.', '|.|', '|_|']];
+            expect(fixtures.buildLcdArray(numberArray,allLcds)).toEqual(lcdArray);
         });
     });
 
     describe('test toLcd',function () {
         it('return correct print',function () {
+            var lcdArray=[['._.', '|_|', '..|'], ['...', '..|', '..|'], ['._.', '|.|', '|_|']];
             spyOn(console,'log');
-            main.printLcd(910);
-            var print='._. ... ._. \n'+'|_| ..| |.| \n'+'..| ..| |_| \n';
+            fixtures.toLcd(lcdArray)
+            var print='._. ... ._. \n'+'|_| ..| |.| \n'+'..| ..| |_| ';
             expect(console.log).toHaveBeenCalledWith(print);
         });
     });
